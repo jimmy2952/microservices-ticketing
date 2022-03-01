d3277a3a4e93016a8673782c2cb82b3d050003a7
jimmy2952 <jimmy2952@gmail.com>
2022-03-02 00:56:38 +0800

    process client

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,5 +1,14 @@
 import 'bootstrap/dist/css/bootstrap.css';
+import buildClient from '../api/build-client';
 
-export default function ({ Component, pageProps }) {
+const AppComponent = ({ Component, pageProps }) => {
   return <Component {...pageProps} />
 };
+
+AppComponent.getInitialProps = async (appContext) => {
+  const client = buildClient(appContext.ctx);
+  const { data } = await client.get('/api/users/currentuser');
+  return data;
+}
+
+export default AppComponent;
--- client/pages/index.js
+++ client/pages/index.js
@@ -1,15 +1,13 @@
-import axios from 'axios';
+import buildClient from '../api/build-client';
 
 const LandingPage = ({ currentUser }) => {
-  console.log(currentUser);
-  // axios.get('/api/users/currentuser').catch(err => console.log(err))
-  return <h1>Landing Page</h1>;
+  return currentUser ? <h1>Your are signed in</h1> : <h1>Your are not signed in</h1>;
 };
 
-LandingPage.getInitialProps = async () => {
-  const response = await axios.get('/api/users/currentuser').catch(err => console.log(err));
-
-  return response.data;
+LandingPage.getInitialProps = async (context) => {
+  const client = buildClient(context);
+  const { data } = await client.get('/api/users/currentuser')
+  return data;
 }
 
 export default LandingPage;
