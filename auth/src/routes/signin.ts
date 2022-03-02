dabf7a1d67de59f201733741202383b18186ee8d
jimmy2952 <jimmy2952@gmail.com>
2022-03-03 00:28:36 +0800

    complete common module

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -4,8 +4,7 @@ import jwt from 'jsonwebtoken';
 
 import { Password } from '../services/password';
 import { User } from '../models/user';
-import { validateRequest } from '../middlewares/validate-request';
-import { BadRequestError } from '../errors/bad-request-error';
+import { validateRequest, BadRequestError } from '@jimmy2952-ticket/common';
 
 const router = express.Router();
 
--- client/pages/index.js
+++ client/pages/index.js
@@ -6,7 +6,7 @@ const LandingPage = ({ currentUser }) => {
 
 LandingPage.getInitialProps = async (context) => {
   const client = buildClient(context);
-  const { data } = await client.get('/api/users/currentuser')
+  const { data } = await client.get('/api/users/currentuser');
   return data;
 }
 
